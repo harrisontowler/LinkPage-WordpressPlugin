9a7689aaca1299ad335db471d819ceea0903556d
harrisontowler <46505212+harrisontowler@users.noreply.github.com>
2023-05-28 14:17:30 +0100

    Update tcu-scripts.js
    3rd button template

--- js/tcu-scripts.js
+++ js/tcu-scripts.js
@@ -173,6 +173,126 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  const tcuForm = document.getElementById('tcu-form');
+  const tcuText = document.getElementById('tcu-text');
+  const tcuUrl = document.getElementById('tcu-url');
+  const tcuColor = document.getElementById('tcu-color');
+  const tcuBorderColor = document.getElementById('tcu-border-color');
+  const tcuDivBgColor = document.getElementById('tcu-div-bg-color');
+  const tcuBorderRadius = document.getElementById('tcu-border-radius');
+  const tcuPreviewElement = document.getElementById('tcu-preview-element');
+
+  if (tcuForm) {
+    // Update the preview element styles based on user input
+    const updatePreviewStyles = () => {
+      tcuPreviewElement.style.color = tcuColor.value;
+      tcuPreviewElement.style.borderColor = tcuBorderColor.value;
+      tcuPreviewElement.style.borderWidth = '2px'; // Add this line
+      tcuPreviewElement.style.borderStyle = 'solid'; // Add this line
+      tcuPreviewElement.style.borderRadius = tcuBorderRadius.value + 'px';
+      tcuPreviewElement.style.backgroundColor = tcuDivBgColor.value;
+      tcuPreviewElement.innerHTML = tcuText.value || 'Text preview';
+    };
+
+    // Set initial preview styles
+    updatePreviewStyles();
+
+    // Add event listeners to the form inputs
+    tcuText.addEventListener('input', updatePreviewStyles);
+    tcuColor.addEventListener('input', updatePreviewStyles);
+    tcuBorderColor.addEventListener('input', updatePreviewStyles);
+    tcuDivBgColor.addEventListener('input', updatePreviewStyles);
+    tcuBorderRadius.addEventListener('input', updatePreviewStyles);
+
+    // Get the button element
+    const button2 = document.getElementById('button2');
+
+    if (button2) {
+        button2.addEventListener('click', function() {
+            // Set the input values to the styles you want
+            tcuText.value = '';
+            tcuColor.value = '#000000';
+            tcuBorderColor.value = '#000000';
+            tcuDivBgColor.value = '#FFFFFF';
+            tcuBorderRadius.value = '30';
+
+            // Dispatch an 'input' event on each of the input elements
+            tcuText.dispatchEvent(new Event('input'));
+            tcuColor.dispatchEvent(new Event('input'));
+            tcuBorderColor.dispatchEvent(new Event('input'));
+            tcuDivBgColor.dispatchEvent(new Event('input'));
+            tcuBorderRadius.dispatchEvent(new Event('input'));
+        });
+    }
+  }
+});
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  const tcuForm = document.getElementById('tcu-form');
+  const tcuText = document.getElementById('tcu-text');
+  const tcuUrl = document.getElementById('tcu-url');
+  const tcuColor = document.getElementById('tcu-color');
+  const tcuBorderColor = document.getElementById('tcu-border-color');
+  const tcuDivBgColor = document.getElementById('tcu-div-bg-color');
+  const tcuBorderRadius = document.getElementById('tcu-border-radius');
+  const tcuPreviewElement = document.getElementById('tcu-preview-element');
+
+  if (tcuForm) {
+    // Update the preview element styles based on user input
+    const updatePreviewStyles = () => {
+      tcuPreviewElement.style.color = tcuColor.value;
+      tcuPreviewElement.style.borderColor = tcuBorderColor.value;
+      tcuPreviewElement.style.borderWidth = '2px'; // Add this line
+      tcuPreviewElement.style.borderStyle = 'solid'; // Add this line
+      tcuPreviewElement.style.borderRadius = tcuBorderRadius.value + 'px';
+      tcuPreviewElement.style.backgroundColor = tcuDivBgColor.value;
+      tcuPreviewElement.innerHTML = tcuText.value || 'Text preview';
+    };
+
+    // Set initial preview styles
+    updatePreviewStyles();
+
+    // Add event listeners to the form inputs
+    tcuText.addEventListener('input', updatePreviewStyles);
+    tcuColor.addEventListener('input', updatePreviewStyles);
+    tcuBorderColor.addEventListener('input', updatePreviewStyles);
+    tcuDivBgColor.addEventListener('input', updatePreviewStyles);
+    tcuBorderRadius.addEventListener('input', updatePreviewStyles);
+
+    // Get the button element
+    const button3 = document.getElementById('button3');
+
+    if (button3) {
+        button3.addEventListener('click', function() {
+            // Set the input values to the styles you want
+            tcuText.value = '';
+            tcuColor.value = '#000000';
+            tcuBorderColor.value = '#000000';
+            tcuDivBgColor.value = '#FFFFFF';
+            tcuBorderRadius.value = '0';
+
+            // Dispatch an 'input' event on each of the input elements
+            tcuText.dispatchEvent(new Event('input'));
+            tcuColor.dispatchEvent(new Event('input'));
+            tcuBorderColor.dispatchEvent(new Event('input'));
+            tcuDivBgColor.dispatchEvent(new Event('input'));
+            tcuBorderRadius.dispatchEvent(new Event('input'));
+        });
+    }
+  }
+});
+
+
+
+
+
+
+
+
 jQuery(document).ready(function($) {
     // Force cursor to be after the "hutl.ink/" text
     $('#tcu-url').on('click', function() {
