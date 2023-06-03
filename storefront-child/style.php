3b36a3f298a8217d21b28421b1ebd0bd24af0411
harrisontowler <46505212+harrisontowler@users.noreply.github.com>
2023-06-03 10:20:02 +0100

    Create style.php
    style

---
+++ storefront-child/style.php
@@ -0,0 +1,57 @@
+/*
+Theme Name: Storefront Child
+Theme URI: https://woocommerce.com/storefront/
+Template: storefront
+Author: Automattic
+Author URI: https://woocommerce.com/
+Description: Storefront is the perfect theme for your next WooCommerce project. Designed and developed by WooCommerce Core developers, it features a bespoke integration with WooCommerce itself plus many of the most popular customer facing WooCommerce extensions. There are several layout &amp; color options to personalise your shop, multiple widget regions, a responsive design and much more. Developers will love its lean and extensible codebase making it a joy to customize and extend. Looking for a WooCommerce theme? Look no further!
+Tags: e-commerce,two-columns,left-sidebar,right-sidebar,custom-background,custom-colors,custom-header,custom-menu,featured-images,full-width-template,threaded-comments,accessibility-ready,rtl-language-support,footer-widgets,sticky-post,theme-options,editor-style
+Version: 4.2.0.1679180171
+Updated: 2023-03-18 22:56:11
+
+*/
+
+
+
+
+@media (max-width: 11000px){
+.dokan-store-wrap.layout-left .dokan-store-sidebar {
+
+    display: none;
+}}
+
+@media (max-width: 11000px){
+.site-header {
+    display:none;
+}}
+@media (max-width: 11000px){
+.storefront-breadcrumb {
+display:none; }}
+
+.dokan-single-store .profile-frame .profile-info-box .profile-info-summery-wrapper .profile-info-summery {
+    width: 100%;
+    padding-top: 20%;
+}
+
+.dokan-single-store .profile-frame .profile-info-box .profile-info-summery-wrapper { border:none;
+
+}
+
+
+@media (min-width: 1200px){
+.dokan-single-store .profile-frame .profile-info-box.profile-layout-layout2 .profile-info-summery-wrapper .profile-info-summery .profile-info {
+  
+    top: 0%!important;
+}}
+
+
+.dokan-single-store .profile-frame .profile-info-box .profile-info-summery-wrapper .profile-info-summery .profile-info-head .profile-img.profile-img-square img {
+border:none;
+border-radius:60px;
+display: block;
+    margin: 0 auto;}
+    
+    .site-footer { background-color:#00000000; padding-top:-10px;
+padding-bottom:-20px}
+.site-info {display:none;}
+.storefront-handheld-footer-bar {display:none;}
