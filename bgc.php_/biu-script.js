731fa5b3be35a9678daf45f5053294905c3b6a8b
harrisontowler <46505212+harrisontowler@users.noreply.github.com>
2023-06-07 01:40:00 +0100

    Update biu-script.js

--- bgc.php_/biu-script.js
+++ bgc.php_/biu-script.js
@@ -17,11 +17,12 @@ jQuery(document).ready(function($) {
 
     if (submitType === 'biu-submit-color' && $('#biu-color').val()) {
       formData.append('biu-color', $('#biu-color').val());
-    }
-
-    if (submitType === 'biu-submit-image' && $('#biu-image').val()) {
+    } else if (submitType === 'biu-submit-image' && $('#biu-image').val()) {
       console.log($('#biu-image')[0].files[0]); // Debugging statement to check the file object
       formData.append('biu-image', $('#biu-image')[0].files[0]);
+    } else if (submitType === 'biu-submit-gradient' && $('#biu-color1').val() && $('#biu-color2').val()) {
+      formData.append('biu-color1', $('#biu-color1').val());
+      formData.append('biu-color2', $('#biu-color2').val());
     }
 
     if (submitType) {
@@ -37,20 +38,31 @@ jQuery(document).ready(function($) {
           console.log(response); // Debugging information
           if (response.success) {
             if (submitType === 'biu-submit-color') {
-              alert('Color selected successfully!');
+              console.log('Color applied successfully!');
+              $('body').css('background', response.data);
+              location.reload();
+            } else if (submitType === 'biu-submit-gradient') {
+              console.log('Gradient applied successfully!');
               location.reload();
             } else if (submitType === 'biu-submit-image') {
-              alert('Image uploaded successfully!');
+              console.log('Image uploaded successfully!');
               location.reload();
             }
+          } else if (response.data && response.data.error) {
+            console.log('Error: ' + response.data.error);
           } else {
-            alert('Error: ' + response.data);
+            console.log('An error occurred while processing the request. Please try again later.');
           }
         },
         error: function(xhr, status, error) {
           console.log(xhr.responseText); // Debugging information
           console.log(status); // Debugging information
           console.log(error); // Debugging information
+          if (submitType === 'biu-submit-gradient' && xhr.status !== 200) {
+            console.log('An error occurred while applying the gradient. Please check the input values and try again.');
+          } else {
+            console.log('An error occurred while processing the request. Please try again later.');
+          }
         }
       });
     }
@@ -58,13 +70,28 @@ jQuery(document).ready(function($) {
 
   // Add an event listener for the color input field's input event
   $('#biu-color').on('input', function() {
-    // If there is a background image, save it and remove it
-    if ($('body').css('background-image') !== 'none') {
-      originalBgImage = $('body').css('background-image');
-      $('body').css('background-image', 'none');
-    }
+    // Remove the gradient background if it exists
+    $('body').css('background', 'none');
 
     // Change the background color of the body to the selected color
     $('body').css('background-color', $(this).val());
+  });
+
+  // Add event listeners for the color inputs of the gradient
+  $('#biu-color1, #biu-color2').on('input', function() {
+    var color1 = $('#biu-color1').val();
+    var color2 = $('#biu-color2').val();
+    var gradientValue = 'linear-gradient(to bottom, ' + color1 + ', ' + color2 + ')';
+
+    // If there is an original background image, remove it
+    if (originalBgImage !== '') {
+      $('body').css('background-image', 'none');
+    }
+
+    // Remove the background color if it exists
+    $('body').css('background-color', 'transparent');
+
+    // Apply the gradient background to the body
+    $('body').css('background', gradientValue);
   });
 });
